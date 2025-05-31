169d968e97bbd3cdaff9c0ddcfa2cf6a9f20e0b9
Sahil <sahilismailnavar@gmail.com>
2025-05-31 15:09:31 +0530

    Edited in the css

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Manager = () => {
     return (
         <>
-            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+            <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
                 <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
             </div>
-            <div className="bg-slate-50 mycontainer">
+            <div className=" mycontainer">
                 <h1 className='text-4xl font-bold text text-center'>
                     <span className='text-green-700 '> &lt;</span>
                     Pass
-                    <span className='text-green-700'>Man/&gt; </span>
+                    <span className='text-green-500'>Man/&gt; </span>
                 </h1>
                 <p className='text-green-700 text-lg text-center'>Your Password Manager</p>
                 <div className="flex flex-col p-4 text-black gap-6 items-center">
