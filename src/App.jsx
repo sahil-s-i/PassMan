12cf445f5d9eacb1f053d55e5a2f9efa2222e890
Sahil <sahilismailnavar@gmail.com>
2025-06-02 13:34:52 +0530

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Manager />
+      <div className="min-h-[90vh]">
+        <Manager />
+      </div>
+      <Footer />
     </>
   )
 }
