33e00817a879905aa3e96556a35005663f294e27
Sahil <sahilismailnavar@gmail.com>
2025-06-02 13:34:16 +0530

    Added footer with the footer content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 w-full'>
+        <p className='text-center text-gray-400 text-sm py-4'>Copyright &copy; 2025, All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
